7555303a63bbe4ba117b5dc9e0e885e4fbf19ef8
aymen600 <alahouel11@gmail.com>
2023-05-19 11:17:59 +0200

    create http server

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,8 +1,35 @@
+// Launcher.java
 package fr.lernejo.navy_battle;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Launcher {
-    public static void main(String[] args) {
-        // Code à exécuter au démarrage du programme
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(1);
+        server.setExecutor(threadPoolExecutor);
+        server.createContext("/ping", new CustomHandler());
+        server.start();
+        System.out.println("Server started on port " + port);
+    }
+
+    static class CustomHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello";
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        }
     }
 }
-
